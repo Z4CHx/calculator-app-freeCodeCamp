b7b0311f830829e7a2137c395dae517dcf6f35ad
Zachary Durland <zacharydurland@gmail.com>
2017-04-13 09:45:50 +0900

    add interface and most basic logic
    add ability to do an operation on two numbers
    -cannot then add onto the result, etc
    -cannot input a long expression and expect order of operations to work

---
+++ main.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+    var a = 0;
+    var b = 0;
+    var result = 0;
+    var pressed = "";
+    var operator = "";
+    var displayArea = $("#display");
+    var display = function(output){
+        $("#display").append(output);
+    }
+    
+    var calc = function(a,b,operator){
+        a = parseFloat(a);
+        b = parseFloat(b);
+        displayArea.text("");
+       if( operator === "+" ) {
+           console.log("result: a + b was " + ( a + b ) );
+           result = a + b;
+           return result;
+       } else if ( operator === "-" ) {
+           console.log("result: a - b was " + ( a - b ) );
+           result = a - b;
+           return result;
+       } else if ( operator === "*" ) {
+           console.log("result: a * b was " + ( a * b ) );
+           result = a * b;
+           return result;
+       } else if ( operator === "/" ) {
+           console.log("result: a / b was " + ( a / b ) );
+           result = a / b;
+           return result;
+       }
+       
+   };
+    
+    
+    
+    $("#numbers a, #operators a").click(function(){
+        var buttonValue = $(this).html();
+//        console.log("button value was " + buttonValue);
+        if (buttonValue === "C"){
+            pressed = "";
+            display(pressed);
+            console.log("was cleared");
+        } else if (buttonValue === "AC"){
+            a = 0;
+            b = 0;
+            result = 0;
+            pressed = "";
+            display(pressed);
+            console.log("all was cleared");
+        } else if (buttonValue === "="){
+            
+            display(calc(a,b,operator)); 
+            console.log("equals pressed");
+        } else if (buttonValue === "+" || buttonValue === "-" || buttonValue === "*" || buttonValue === "/") {
+            operator = buttonValue;
+            display(operator);
+        } else {
+            pressed = $(this).html();
+            if (operator === "") {
+                a += pressed;
+            } else {
+                b = pressed;
+            }
+            display(pressed);
+//            console.log("went to else");
+        } 
+        
+        console.log("a is " + a + ", b is " + b + ", and operator is " + operator);
+    });
+
+    
+   
+    
+});
